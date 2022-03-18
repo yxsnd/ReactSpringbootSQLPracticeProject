b8de0f272182d7282bf4456675b46865955d2c1e
yxsnd <yasindu.d@hotmail.com>
2022-03-18 13:49:21 -0400

    added base packages

--- src/main/java/com/ykd/calender/ReactSpringbootSqlPracticeProjectApplication.java
+++ src/main/java/com/ykd/calender/ReactSpringbootSqlPracticeProjectApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.ykd.calender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class ReactSpringbootSqlPracticeProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReactSpringbootSqlPracticeProjectApplication.class, args);
 	}
-
 }
